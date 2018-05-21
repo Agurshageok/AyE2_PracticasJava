4947140c119586220b7ebac5468bcfcc61f4d60f
Agurshageok <Docente@UCAECE005>
2018-05-21 09:47:17 -0300

    Primer Commit! :D

---
+++ AyE2_Practicas/src/ayE2_Practicas/Practica6/Lamparita.java
@@ -0,0 +1,18 @@
+package ayE2_Practicas.Practica6;
+
+public class Lamparita {
+	private boolean es;
+	public Lamparita() {
+		es = false;
+	}
+	public Lamparita(boolean x) {
+		es = x;
+	}
+	public void encender() {this.es=true;}
+	public void apagar() {this.es=false;}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
